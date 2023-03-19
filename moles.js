220c76d506aff14e593f709528595bfa93bc1db4
Margarita Medinskaia <89798767+MarMedinskaia@users.noreply.github.com>
2023-03-18 22:08:45 -0700

    Update moles.js

--- moles.js
+++ moles.js
@@ -2,7 +2,7 @@ let c = document.getElementById('myCanvas');
 let ctx = c.getContext('2d');
 c.addEventListener('click', click); //When a player clicks on the field of the game (canvas), the click is evaluated with click(e) function.  
 let bn1 = document.getElementById('buttonStart');
-bn1.addEventListener('click', startGame); //When the button "Start" is clicked, the function startGame() is called and so the process begins.
+bn1.addEventListener('click', startGame); //When the button "Start" is clicked, the function startGame() is called and the game begins.
 let level = document.getElementById('level');
 let score = document.getElementById('score');
 let table = document.getElementById('scoreboard');
@@ -32,12 +32,12 @@ function startGame() {
     alert('Level 1');
     clearInterval(timerID);
     timerID = null;
-    timerID = setInterval(()=>appear(2,1000,1), 2000);  //The moles on level 1 will appear with the interval of 2 seconds. 
+    timerID = setInterval(()=>appear(8,1000,1), 2000);  //8 moles on level 1 will appear with the interval of 2 seconds. 
 }
 
 function appear(mCount, showTime, level) {  
     count++;
-    if (count <= mCount) { //8 moles will appear on level 1.
+    if (count <= mCount) { 
         let rand = Number(Math.random()); //The place where a mole appears will be unpredictably chosen between 5 locations based on a generated random number. 
         //5 possible locations where a mole is drawn.
         if (rand >= 0.8) {
@@ -60,7 +60,7 @@ function appear(mCount, showTime, level) {
         ctx.clearRect(0, 0, 600, 400);
         moleNum = 0;
         }, showTime);
-    //After all 8 moles are drawn, going to the next level.
+    //Going to the next level.
     } else {
         count = 0;
         clearInterval(timerID);
@@ -92,19 +92,19 @@ function createMole(x,y) {
 function level2() {
     level.innerHTML = 2;
     if (timerID == null) {
-        timerID = setInterval(()=>appear(2,800,2), 1400); //The moles on level 2 will appear with the interval of 1.4 second. 
+        timerID = setInterval(()=>appear(10,800,2), 1400); //10 moles on level 2 will appear with the interval of 1.4 sec 
     }
 }
 
 function level3() {
     level.innerHTML = 3;
     if (timerID == null) {
-        timerID = setInterval(()=>appear(2,600,3), 1000); //The moles on level 2 will appear with the interval of 1 second. 
+        timerID = setInterval(()=>appear(12,600,3), 1000); //12 moles on level 3 will appear with the interval of 1 sec 
     }
 }
 
 function end() {
-    //The username entered in the beggining of the game is compared with those in the table (scoreboard).
+    //The username entered in the beggining of the game is compared with those in the table (scoreboard)
     let exists = false;
     let i=0;
     while (!exists && i < users.length) {
@@ -161,11 +161,10 @@ function sortTable() {
     }
 }
 
-//The function is called when the game field (canvas) is clicked. 
+//The location of the click is compared with the location of the mole that is on the screen at the moment. If they match, 1 score is added.
 function click(e) {
     let clickX = e.offsetX;
     let clickY = e.offsetY;
-    //The location of the click is compared with the location of the mole that is on the screen at the moment. If they match, 1 score is added.
     if (((moleNum == 1 && clickX <= (posX+76) && clickX >= posX && clickY <= (posY+57) && clickY >= posY)) || (moleNum == 2 && clickX <= (posX+76) && clickX >= posX && clickY <= (posY+57) && clickY >= posY) || 
     moleNum == 3 && (clickX <= (posX+76) && clickX >= posX && clickY <= (posY+57) && clickY >= posY) || (moleNum == 4 && clickX <= (posX+76) && clickX >= posX 
     && clickY <= (posY+57) && clickY >= posY) || (moleNum == 5 && clickX <= (posX+76) && clickX >= posX && clickY <= (posY+57) && clickY >= posY)) {
